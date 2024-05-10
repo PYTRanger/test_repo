d3d761f3602b4138af8db709eff148df347766d8
Meyank <meyank24@gmail.com>
2024-05-11 00:59:19 +0530

    initial commit

---
+++ breakcontinue.py
@@ -0,0 +1,13 @@
+for i in range(15):
+    if i ==10:
+        print('Nigga we got the 10 now exit')
+        break
+    print(i)
+
+
+
+
+for i in range(12):
+    if(i==20):
+        print('Skip the iteration')
+        continue
---
+++ function.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    g = (a*b)/(a+b)
+    print(g)
+
+
+def isgreater(a,b):
+    if a>b:
+        print("A is bigger")
+    else:
+        print('B is bigger')
+
+
+
+add(12,38)
+
+
+a = 12
+b = 13
+
+isgreater(a,b)
+
