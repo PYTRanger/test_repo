82eb0e6f8b70a33aa08981b89ccb1313f8c74dbe
Meyank <meyank24@gmail.com>
2024-05-12 12:55:31 +0530

    daily commmit

---
+++ Day5.py
@@ -0,0 +1,10 @@
+#print('Hey I am good boy \n and Someone is a good girl also')
+
+
+#comments
+# backslash n used to get new line \n
+
+#escape sequence character use backslash to use excase sequence characters
+
+
+print('hello world', 9,5,6 ,'hey', sep='`')
