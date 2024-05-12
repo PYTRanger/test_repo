732962a74575b8538da3d6c0233bb4b966aa2951
PYTRanger <1406utkarsh@gmail.com>
2024-05-12 15:43:34 +0530

    Automated commit: set.py

---
+++ set.py
@@ -0,0 +1,10 @@
+#set is one of the most used data type in api's
+
+
+sets = {'meyank':2400,'kunal':7}
+print(sets.keys)
+
+kunal = set()# to create a empty this is the syntax for it
+
+for keys in sets:
+    print(keys)
